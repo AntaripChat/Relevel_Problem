4087d1623a54f59dc1f19e5a1fe4110d86c712f6
Antarip chatterjee <85601268+AntaripChat@users.noreply.github.com>
2022-07-05 21:24:16 +0530

    Create 2darrSum.js

---
+++ Problem/2darrSum.js
@@ -0,0 +1,9 @@
+let arr = [[1,2,3],[3,4,5]]
+let sum = 0;          
+for(let i = 0; i < arr.length;i++){
+    for(let j = 0 ; j < arr[i].length; j ++){
+        //console.log(arr[i],[j]);
+        sum = sum + arr[i][j];
+    }
+}
+console.log(sum);
